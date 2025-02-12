6686b798c6a10a6d8b0fabc68abfcb4673750692
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2025-02-12 18:19:35 -0500

    fix: :bug: change ip

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 
 	// Configurar CORS
 	corsHandler := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://44.211.148.80:3000"},               // Permite solicitudes desde tu frontend
+		AllowedOrigins: []string{"http://3.229.231.204:3000"},               // Permite solicitudes desde tu frontend
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Métodos HTTP permitidos
 		AllowedHeaders: []string{"Content-Type", "Authorization"},           // Encabezados permitidos
 	})
